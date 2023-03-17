b7480fa0919798838b9d8fe398cfcc132e1c1339
azimust <woody.azimbek@gmail.com>
2023-03-17 21:07:43 +0600

    refactor: ui and js code

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,35 +1,33 @@
-function portfolioWorks(title, href){
-    let portfolio__row2 = document.querySelector('.portfolio__row-2');
-    let block = document.createElement('div');
-    let blockTitle = document.createElement('h3');
-    let blockHref = document.createElement('a');
-    let demo = document.createElement('div');
-    let demoText = document.createElement('h3');
-
-    portfolio__row2.appendChild(block)
-    block.appendChild(blockTitle);
-    block.appendChild(blockHref);
-    blockHref.appendChild(demo);
-    demo.appendChild(demoText);
-
-    block.classList.add('block');
-    blockTitle.classList.add('block__title');
-    blockHref.classList.add('block__href');
-    demo.classList.add('block__item');
-    demoText.classList.add('demo');
-
-    blockTitle.innerHTML = title;
-    blockHref.href = href;
-    demoText.innerHTML = 'TAP TO DEMO';
+let portfolioData = [
+    {title: 'stop-watch', ref: 'https://azimust.github.io/js-stopwatch/', img: './portfolio-imgs/watch.png'},
+    {title: 'plants-landing', ref: 'https://azimust.github.io/plants-landing/', img: './portfolio-imgs/plants.png'},
+    {title: 'quote-generator', ref: 'https://azimust.github.io/quote-generator/', img: './portfolio-imgs/quote.png'},
+    {title: 'movie-app', ref: 'https://azimust.github.io/movie-app/', img: './portfolio-imgs/movie.png'},
+    {title: 'github-profiles', ref: 'https://azimust.github.io/github-profiles/', img: './portfolio-imgs/prof.png'},
+    {title: 'momentum', ref: 'https://azimust.github.io/momentum/', img: './portfolio-imgs/mom.png'},
+    {title: 'code-editor', ref: 'https://azimust.github.io/code-editor-js/', img: './portfolio-imgs/edit.png'},
+    {title: 'Binary', ref: 'https://azimust.github.io/binary-landing/', img: './portfolio-imgs/binary.png'},
+    {title: 'Pixel-azim', ref: 'https://azimust.github.io/pixelazim-site/', img: './portfolio-imgs/pix.png'},
+    {title: 'Expo-forum', ref: 'https://azimust.github.io/expoforum/', img: './portfolio-imgs/expo.png'},
+    {title: 'denisnovik-landing', ref: 'https://azimust.github.io/denisnovik-landing/', img: './portfolio-imgs/den.png'},
+];
+let portfolioRow = document.querySelector('.portfolio__row-2')
+function portfolioWorks(){
+    portfolioData.map((e) => {
+        portfolioRow.innerHTML += `
+            <div class="block">
+                <h3 class="block__title">${e.title}</h3>
+                <a class="block__href" href="${e.ref}">
+                    <div class="block__item">
+                        <div class="demo" style="background: url(${e.img}) no-repeat;
+                            background-size: cover;
+                            background-position: 0%;">
+                            <h3>TAP TO DEMO</h3>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `
+    })
 }
-portfolioWorks('Binary', "https://azimust.github.io/binary-landing/");
-portfolioWorks('Pixel-azim', "https://azimust.github.io/pixelazim-site/");
-portfolioWorks('Expo-forum', "https://azimust.github.io/expoforum/");
-portfolioWorks('denisnovik', "https://azimust.github.io/denisnovik-landing/");
-portfolioWorks('simple-portfolio', "https://azimust.github.io/simple-portfolio/");
-portfolioWorks('check-bs-5', "https://azimust.github.io/check-bs-5/");
-portfolioWorks('stop-watch', "https://azimust.github.io/js-stopwatch/");
-portfolioWorks('plants-landing', "https://azimust.github.io/plants-landing/");
-portfolioWorks('quote-generator', "https://azimust.github.io/quote-generator/");
-portfolioWorks('todo-list', "https://azimust.github.io/todo-list/");
-portfolioWorks('code-editor', "https://azimust.github.io/code-editor-js/");
+portfolioWorks();
